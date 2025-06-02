9e12f1ef1d2364be589ea1514afb9189ad812f47
Ricardo Rosendo <ricardo_rosendo@ymail.com>
2025-06-02 09:55:42 -0300

    spring boot project

---
+++ src/main/java/com/RicardoRosendo/ProjectSimples/ProjectSimplesApplication.java
@@ -0,0 +1,13 @@
+package com.RicardoRosendo.ProjectSimples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSimplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSimplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RicardoRosendo/ProjectSimples/ProjectSimplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RicardoRosendo.ProjectSimples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSimplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
