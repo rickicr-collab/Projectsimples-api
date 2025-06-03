1bef396b2ffa412cb778ef9274bf7785d2df1014
Ricardo Rosendo <ricardo_rosendo@ymail.com>
2025-06-03 10:52:35 -0300

    Create interface name TaskRepository.Java

---
+++ src/main/java/com/RicardoRosendo/ProjectSimples/repositories/TaskRepository.java
@@ -0,0 +1,19 @@
+package com.RicardoRosendo.ProjectSimples.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.RicardoRosendo.ProjectSimples.models.Task;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    
+    List<Task> findByUser_Id(Long id);
+
+    //@Query("SELECT t FROM Task t WHERE t.user.id = :id")
+    //List<Task> findByUserId(@Param("id") Long id);
+
+    //@Query(value = "SELECT * FROM task t WHERE t.user_id = :id", nativeQuery = true)
+    //List<Task> findByUserId(@Param("id") Long id);
+
+}
