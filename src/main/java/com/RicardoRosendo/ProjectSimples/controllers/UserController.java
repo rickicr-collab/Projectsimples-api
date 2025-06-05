c86c575d9ea14a8a22a75cfc842db1499a1377a9
Ricardo Rosendo <ricardo_rosendo@ymail.com>
2025-06-05 10:03:37 -0300

    Create class java name UserController and modifield file Users.java

---
+++ src/main/java/com/RicardoRosendo/ProjectSimples/controllers/UserController.java
@@ -0,0 +1,63 @@
+package com.RicardoRosendo.ProjectSimples.controllers;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.RicardoRosendo.ProjectSimples.models.Users;
+import com.RicardoRosendo.ProjectSimples.services.UserService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/users")
+@Validated
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("{id}")
+    public ResponseEntity<Users> findById(@PathVariable Long id){
+        Users user = this.userService.findById(id);
+        return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    @Validated(Users.CreateUser.class)
+    public ResponseEntity<Void> create(@Valid @RequestBody Users obj) {
+        obj = this.userService.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(obj.getId())
+                .toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+
+    @PutMapping("/{id}")
+    @Validated(Users.UpdateUser.class)
+    public ResponseEntity<Void> update(@Valid @RequestBody Users obj, @PathVariable Long id){
+        obj.setId(id);
+        obj = this.userService.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        this.userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
