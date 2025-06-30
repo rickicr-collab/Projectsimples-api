54963bdd850ee78b3deccbcf1f12458915d799c9
Ricardo Rosendo <ricardo_rosendo@ymail.com>
2025-06-30 10:17:22 -0300

    create files java  class name UserDtaisServiceIMpl and Class JWTUtil and refatored existing files

---
+++ src/main/java/com/RicardoRosendo/ProjectSimples/Security/JWTUtil.java
@@ -0,0 +1,5 @@
+package com.RicardoRosendo.ProjectSimples.Security;
+
+public class JWTUtil {
+
+}
---
+++ src/main/java/com/RicardoRosendo/ProjectSimples/services/UserDetailsServiceImpl.java
@@ -0,0 +1,5 @@
+package com.RicardoRosendo.ProjectSimples.services;
+
+public class UserDetailsServiceImpl {
+
+}
